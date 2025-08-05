bcfd5594af7884d5feaaa6d756a1eab7b70292d4
Vivek Mane <157614039+vivek-9941@users.noreply.github.com>
2025-08-05 09:45:04 +0530

    Time: 5 ms (53.42%) | Memory: 10.1 MB (33.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -4,21 +4,14 @@ public:
         int n = s1.size();
         int m = s2.size();
         if (n > m) return false;
-        unordered_map<char, int> mpp;
-        for (char c : s1) mpp[c]++;
-        for (int i = 0; i <= m - n; i++) {
-            unordered_map<char, int> k = mpp;
-            bool isPermutation = true;
-            for (int j = i; j < i + n; j++) {
-                if (k.find(s2[j]) == k.end()) {
-                    isPermutation = false;
-                    break;
-                }
-                k[s2[j]]--;
-                if (k[s2[j]] == 0) k.erase(s2[j]);
-            }
-
-            if (isPermutation && k.empty()) return true;
+        vector<int> freq1(26 , 0), freq2(26,0);
+        for (char c : s1) freq1[c - 'a']++;
+        for (int i = 0; i < m ; i++) {
+          freq2[s2[i] -'a']++;
+          if(i >= n){
+            freq2[s2[i-n] - 'a']--;
+          }
+          if(freq1 == freq2)return true;
         }
 
         return false;
